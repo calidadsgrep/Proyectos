1fb207c9ce097da809fac3c88981990870eb266f
soporte-sg <firmacalidadsg1@gmail.com>
2022-05-28 09:40:23 -0500

    actualizando base de datos

--- models/Database.php
+++ models/Database.php
@@ -4,7 +4,7 @@ class Database
     public static function StartUp()
     {
         //$pdo = new PDO('mysql:host=localhost;dbname=woodensp_sgvalle;charset=utf8', 'woodensp_sgvalle', 'eAi3NOPAF,G_');
-        $pdo = new PDO('mysql:host=localhost;dbname=proyecto;charset=utf8', 'root', '');
+        $pdo = new PDO('mysql:host=localhost;dbname=proyectos;charset=utf8', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
